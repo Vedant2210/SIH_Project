50f962b8434c7e1aa2d0236f6acbeedadbf5a576
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-09-21 21:30:40 +0530

    Add files

--- backend/routes/animalRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerAnimal } = require("../controllers/animalController");
-
-// POST /api/animals/register
-router.post("/register", registerAnimal);
-
-module.exports = router;
